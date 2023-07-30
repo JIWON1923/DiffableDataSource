f7efdb64f55b3a5753961341c2e3357c6a79e80d
JIWON1923 <zest1923@gmail.com>
2023-07-31 00:27:11 +0900

    [Feat] 간단한 TableView 구현

--- UITableViewDiffableDataSource/UITableViewDiffableDataSource/ViewController.swift
+++ UITableViewDiffableDataSource/UITableViewDiffableDataSource/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var data = ["1", "2", "3"]
 
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "MyCell")
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+}
