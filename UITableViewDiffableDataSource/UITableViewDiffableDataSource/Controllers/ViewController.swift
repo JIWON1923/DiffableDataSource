6071dca3fbc78afe28738f433005aa64309c99a8
JIWON1923 <zest1923@gmail.com>
2023-07-31 00:46:00 +0900

    [Feat] ShortcutsZip Section Data 추가

--- UITableViewDiffableDataSource/UITableViewDiffableDataSource/Controllers/ViewController.swift
+++ UITableViewDiffableDataSource/UITableViewDiffableDataSource/Controllers/ViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private var data: [Shortcut] = [
-        Shortcut(title: "단축어1", subTitle: "단축어를 써보세요"),
-        Shortcut(title: "단축어2", subTitle: "쉽게 써 보자"),
-        Shortcut(title: "단축어3", subTitle: "꿀도 좀 빨아보자")
+    private var data: [[Shortcut]] = [
+        [
+            Shortcut(title: "단축어1", subTitle: "단축어를 써보세요"),
+            Shortcut(title: "단축어2", subTitle: "쉽게 써 보자"),
+            Shortcut(title: "단축어3", subTitle: "꿀도 좀 빨아보자")
+        ],
+        [
+            Shortcut(title: "단축어4", subTitle: "단축어를 써보세요"),
+            Shortcut(title: "단축어5", subTitle: "쉽게 써 보자"),
+            Shortcut(title: "단축어6", subTitle: "꿀도 좀 빨아보자")
+        ],
+        [
+            Shortcut(title: "단축어7", subTitle: "단축어를 써보세요"),
+            Shortcut(title: "단축어8", subTitle: "쉽게 써 보자"),
+            Shortcut(title: "단축어9", subTitle: "꿀도 좀 빨아보자")
+        ],
     ]
 
     @IBOutlet weak var tableView: UITableView! {
@@ -28,13 +40,17 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         data.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ShortcutTableViewCell.id, for: indexPath) as? ShortcutTableViewCell else { return UITableViewCell() }
-        cell.config(with: data[indexPath.row])
+        cell.config(with: data[indexPath.section][indexPath.row])
         return cell
     }
 }
