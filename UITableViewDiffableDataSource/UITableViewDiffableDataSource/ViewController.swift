6fba62d97f4547af85f111d3db97501348df850e
JIWON1923 <zest1923@gmail.com>
2023-07-31 00:19:27 +0900

    [Chore] tableview diffable datasource project setting

---
+++ UITableViewDiffableDataSource/UITableViewDiffableDataSource/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewDiffableDataSource
+//
+//  Created by 이지원 on 2023/07/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
