ebdf6899d587ca622eeffaf4c071d759e930c7d0
JIWON1923 <zest1923@gmail.com>
2023-07-31 01:59:49 +0900

    [Feat] 간단한 DiffableDatasource 구현

--- UITableViewDiffableDataSource/UITableViewDiffableDataSource/Controllers/ViewController.swift
+++ UITableViewDiffableDataSource/UITableViewDiffableDataSource/Controllers/ViewController.swift
@@ -8,64 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private var headerData = ["카테고리", "어쩌고", "저쩌고"]
-    private var data: [[Shortcut]] = [
-        [
-            Shortcut(title: "단축어1", subTitle: "단축어를 써보세요"),
-            Shortcut(title: "단축어2", subTitle: "쉽게 써 보자"),
-            Shortcut(title: "단축어3", subTitle: "꿀도 좀 빨아보자")
-        ],
-        [
-            Shortcut(title: "단축어4", subTitle: "단축어를 써보세요"),
-            Shortcut(title: "단축어5", subTitle: "쉽게 써 보자"),
-            Shortcut(title: "단축어6", subTitle: "꿀도 좀 빨아보자")
-        ],
-        [
-            Shortcut(title: "단축어7", subTitle: "단축어를 써보세요"),
-            Shortcut(title: "단축어8", subTitle: "쉽게 써 보자"),
-            Shortcut(title: "단축어9", subTitle: "꿀도 좀 빨아보자")
-        ],
-    ]
-
-    @IBOutlet weak var tableView: UITableView! {
-        didSet {
-            tableView.dataSource = self
-            tableView.delegate = self
+    
+    enum Section {
+        case category
+        case popular
+        case latest
+        
+        var headerTitle: String {
+            switch self {
+            case .category:
+                return "카테고리 별 단축어"
+            case .popular:
+                return "인기있는 단축어"
+            case .latest:
+                return "최신 단축어"
+            }
         }
     }
     
+    private var dataSource: UITableViewDiffableDataSource<Section, Shortcut>!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupDataSource()
+        setSnapshot()
         
     }
-}
-
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        data.count
+    
+    private func setupDataSource() {
+        
+        dataSource = UITableViewDiffableDataSource<Section, Shortcut>(tableView: tableView) { tableView, indexPath, item in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: ShortcutTableViewCell.id, for: indexPath) as? ShortcutTableViewCell else { return UITableViewCell() }
+            cell.config(with: item)
+            return cell
+        }
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        data[section].count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: ShortcutTableViewCell.id, for: indexPath) as? ShortcutTableViewCell else { return UITableViewCell() }
-        cell.config(with: data[indexPath.section][indexPath.row])
-        return cell
-    }
-    
-    /*
-     /// 기본 header 사용
-     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-     headerData[section]
-     }
-     */
-    
-    /// custom table view cell을 이용해서 header view 사용
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomHeaderView.id) as? CustomHeaderView else { return nil }
-        cell.config(title: headerData[section])
-        return cell
+    private func setSnapshot()  {
+        let category = [
+            Shortcut(title: "단축어1", subTitle: "단축어를 써보세요"),
+            Shortcut(title: "단축어2", subTitle: "쉽게 써 보자"),
+            Shortcut(title: "단축어3", subTitle: "꿀도 좀 빨아보자")
+        ]
+        
+        let popular = [
+            Shortcut(title: "단축어4", subTitle: "단축어를 써보세요"),
+            Shortcut(title: "단축어5", subTitle: "쉽게 써 보자"),
+            Shortcut(title: "단축어6", subTitle: "꿀도 좀 빨아보자")
+        ]
+        
+        let latest = [
+            Shortcut(title: "단축어7", subTitle: "단축어를 써보세요"),
+            Shortcut(title: "단축어8", subTitle: "쉽게 써 보자"),
+            Shortcut(title: "단축어9", subTitle: "꿀도 좀 빨아보자")
+        ]
+        
+        var snapshot = dataSource.snapshot()
+        snapshot.appendSections([.category, .latest, .popular])
+        snapshot.appendItems(category, toSection: .category)
+        snapshot.appendItems(popular, toSection: .popular)
+        snapshot.appendItems(latest, toSection: .latest)
+        dataSource.apply(snapshot)
     }
 }
--- UITableViewDiffableDataSource/UITableViewDiffableDataSource/Models/Shortcut.swift
+++ UITableViewDiffableDataSource/UITableViewDiffableDataSource/Models/Shortcut.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Shortcut {
+struct Shortcut: Hashable {
     let id: String?
     let title: String?
     let subTitle: String?
